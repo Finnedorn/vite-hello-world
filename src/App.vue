dc378a1375d7e71b3ae226ba9df29ddfe4912cf9
Finnedorn <daniele.giuntoli@outlook.com>
2023-11-08 16:53:33 +0100

    added app source files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppTitle/>
+</template>
+
+<script>
+  import AppTitle from './components/icons/AppTitle.vue';
+  export default {
+    name: 'App',
+    components: {
+      AppTitle
+    },
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/icons/AppTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <h1>
+            {{title}}
+        </h1>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                title: 'la mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+h1 {
+    color: red;
+}
+</style>
